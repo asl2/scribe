b27c4898c04013df8d582cd666149134407c7691
root <root@domU-12-31-39-0B-24-65.compute-1.internal>
2010-04-23 17:52:23 +0000

    Include hour in base filename for ROLL_HOURLY; include hour, minute in base filename if ROLL_OTHER and period is less than 1 hour

--- src/store.cpp
+++ src/store.cpp
@@ -380,6 +380,12 @@ string FileStoreBase::makeBaseFilename(struct tm* creation_time) {
     filename << '-' << creation_time->tm_year + 1900  << '-'
              << setw(2) << setfill('0') << creation_time->tm_mon + 1 << '-'
              << setw(2) << setfill('0')  << creation_time->tm_mday;
+    if (rollPeriod == ROLL_HOURLY) {
+      filename << '-' << setw(2) << setfill('0')  << creation_time->tm_hour;
+    } else if (rollPeriod == ROLL_OTHER && rollPeriodLength < 3600) {
+      filename << '-' << setw(2) << setfill('0')  << creation_time->tm_hour 
+               << '-' << setw(2) << setfill('0')  << creation_time->tm_min;
+    }
 
   }
   return filename.str();
